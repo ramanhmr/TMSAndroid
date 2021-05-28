21e023d79ab4559f7c214b9fc81d118d6f3c4342
Raman Hamarnik <81304947+ramanhmr@users.noreply.github.com>
2021-05-28 21:53:07 +0300

    Homework 5 Kotlin

---
+++ app/src/main/java/com/ramanhmr/tmsandroid/homework6/Homework6Activity.kt
@@ -0,0 +1,12 @@
+package com.ramanhmr.tmsandroid.homework6
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.ramanhmr.tmsandroid.R
+
+class Homework6Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_homework6)
+    }
+}
