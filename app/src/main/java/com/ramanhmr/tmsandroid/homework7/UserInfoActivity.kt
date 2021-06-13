1ec920c3d0de513d1a7e1ec0e9fb87181a276c57
Raman Hamarnik <81304947+ramanhmr@users.noreply.github.com>
2021-06-13 17:04:12 +0300

    Homework 9 Kotlin

--- app/src/main/java/com/ramanhmr/tmsandroid/homework7/UserInfoActivity.kt
+++ app/src/main/java/com/ramanhmr/tmsandroid/homework7/UserInfoActivity.kt
@@ -2,6 +2,8 @@ package com.ramanhmr.tmsandroid.homework7
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.ramanhmr.tmsandroid.R
 import com.ramanhmr.tmsandroid.databinding.ActivityHw7UserInfoBinding
 
 class UserInfoActivity : AppCompatActivity() {
@@ -14,5 +16,14 @@ class UserInfoActivity : AppCompatActivity() {
         binding.tvLogin.text = intent.getStringExtra(CountdownActivity.LOGIN_KEY)
         binding.tvPassword.text = intent.getStringExtra(CountdownActivity.PASSWORD_KEY)
         binding.btnClose.setOnClickListener { this.finish() }
+        Glide
+            .with(this)
+            .load(IMAGE_ADDRESS)
+            .placeholder(R.drawable.ic_baseline_account_circle_24)
+            .into(binding.ivProfilePicture)
+    }
+
+    companion object {
+        private const val IMAGE_ADDRESS = "https://thispersondoesnotexist.com/image"
     }
 }
