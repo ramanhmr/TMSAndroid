b9fb2a9e27cd022a7dad7f129f6e23592763f657
Raman <raman.hamarnik@gmail.com>
2021-06-24 22:11:21 +0300

    Небольшие изменения для более красивого кода

--- app/src/main/java/com/ramanhmr/tmsandroid/MainActivity.kt
+++ app/src/main/java/com/ramanhmr/tmsandroid/MainActivity.kt
@@ -2,28 +2,32 @@ package com.ramanhmr.tmsandroid
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.button.MaterialButton
 import com.ramanhmr.tmsandroid.databinding.ActivityMainBinding
 import com.ramanhmr.tmsandroid.homework10.CandiesActivity
 import com.ramanhmr.tmsandroid.homework6.FlagsActivity
 import com.ramanhmr.tmsandroid.homework7.CountdownActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnFlags.setOnClickListener {
-            startActivity(Intent(this, FlagsActivity::class.java))
-        }
-        binding.btnCountdown.setOnClickListener {
-            startActivity(Intent(this, CountdownActivity::class.java))
-        }
+        binding.btnFlags.setOnClickListener(this)
+        binding.btnCountdown.setOnClickListener(this)
+        binding.btnCandies.setOnClickListener(this)
+    }
 
-        binding.btnCandies.setOnClickListener {
-            startActivity(Intent(this, CandiesActivity::class.java))
+    override fun onClick(view: View?) {
+        when (view?.id) {
+            R.id.btn_candies -> startActivity(Intent(view.context, CandiesActivity::class.java))
+            R.id.btn_countdown -> startActivity(Intent(view.context, CountdownActivity::class.java))
+            R.id.btn_flags -> startActivity(Intent(view.context, FlagsActivity::class.java))
         }
     }
 }
