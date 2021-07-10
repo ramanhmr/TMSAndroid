fc202c414bdbe2c6e4f343a1412b48fcc06ab6b0
Raman <raman.hamarnik@gmail.com>
2021-07-10 10:57:15 +0300

    Homework 16 Android Added divider

--- app/src/main/java/com/ramanhmr/tmsandroid/homework16/MessagesActivity.kt
+++ app/src/main/java/com/ramanhmr/tmsandroid/homework16/MessagesActivity.kt
@@ -3,9 +3,12 @@ package com.ramanhmr.tmsandroid.homework16
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.res.ResourcesCompat
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.ramanhmr.tmsandroid.HomeworkApp
+import com.ramanhmr.tmsandroid.R
 import com.ramanhmr.tmsandroid.databinding.ActivityMessagesBinding
 import com.ramanhmr.tmsandroid.homework16.database.MessageRepository
 
@@ -17,12 +20,15 @@ class MessagesActivity : AppCompatActivity() {
         )
     }
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMessagesBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val dividerItemDecoration = DividerItemDecoration(this, DividerItemDecoration.VERTICAL)
+        ResourcesCompat.getDrawable(resources, R.drawable.divider_black, null)
+            ?.let { dividerItemDecoration.setDrawable(it) }
+        binding.rvMessages.addItemDecoration(dividerItemDecoration)
         binding.rvMessages.layoutManager =
             LinearLayoutManager(this, RecyclerView.VERTICAL, false)
         val adapter = MessageAdapter(
